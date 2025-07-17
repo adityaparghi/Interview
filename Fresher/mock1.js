1a9d203630ae8339fc75c3809ba589aecb63450a
Aditya <adityaparghi26@gmail.com>
2025-07-17 21:23:59 +0530

    mock added

---
+++ Fresher/mock1.js
@@ -0,0 +1,25 @@
+arr0 = [,,,];
+arr1 = [1, 2, 3, 4, 5];
+arr2 = [[1,2,3], [4,5,6], [7,8,9]];
+
+console.log(arr0.length); // 3
+console.log(arr1.length); // 5
+console.log(arr2.length); // 3
+
+// let a = [undefined, undefined, undefined];
+// let b = [,,,];
+
+// console.log(a.length); // 3
+// console.log(b.length); // 3
+
+// console.log(a[0]); // undefined
+// console.log(b[0]); // undefined (but technically it's an empty slot)
+
+// console.log(0 in a); // true — index 0 exists
+// console.log(0 in b); // false — index 0 is empty
+
+// Question 2
+
+// let arr4 = [1,18,9,81,98,124,78];
+// arr4.sort((a,b) => a-b); // ascending order
+// arr4.sort((a,b) => b-a); // descending order
